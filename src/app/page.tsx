507d82e9273c4d472b1ab6963e8c4f0dfeeeb579
MelzCodes <matteo.melis2012@gmail.com>
2024-04-21 12:06:01 +0100

    Adding toasts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function Home() {
   return (
-    <div>
-      <Button className="m-5">Click me</Button>
-    </div>
+    <>
+      <Button
+        className="m-5"
+        onClick={() =>
+          toast.success("Thanks for signing up ❤️", {
+            description: "Enjoy all your free resources! 🎉",
+          })
+        }
+      >
+        Show Toast
+      </Button>
+    </>
   );
 }
