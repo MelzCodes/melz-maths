5c98a9c0dfcd7c40b1c93b91f9813f23d56dd047
MelzCodes <matteo.melis2012@gmail.com>
2024-04-27 18:15:51 +0100

    Adding template Hero and video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@
 import { Button } from "@/components/ui/button";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import { toast } from "sonner";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
 
 const testimonials = [
   {
@@ -41,21 +43,50 @@ const testimonials = [
 export default async function Home() {
   return (
     <>
-      <Button
-        className="m-5"
-        onClick={() =>
-          toast.success("Thanks for signing up ❤️", {
-            description: "Enjoy all your free resources! 🎉",
-          })
-        }
-      >
-        Show Toast
-      </Button>
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
+      <div>
+        <motion.h1
+          initial={{
+            opacity: 0,
+            y: 20,
+          }}
+          animate={{
+            opacity: 1,
+            y: [20, -5, 0],
+          }}
+          transition={{
+            duration: 0.5,
+            ease: [0.4, 0.0, 0.2, 1],
+          }}
+          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
+        >
+          Providing all the Leaving Cert Maths <br></br>{" "}
+          <Highlight className="text-black dark:text-white my-2">
+            Resources You Need
+          </Highlight>
+        </motion.h1>
+      </div>
+      <div className="flex justify-center items-center py-5">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/9a2mLY8o7Mo?si=qDXlGroXn3PdDdvb"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold text-center my-10">
+          What the Students Have to Say...
+        </h1>
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
     </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,8 @@ const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
+const svgToDataUri = require("mini-svg-data-uri");
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -85,7 +87,22 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+  plugins: [
+    require("tailwindcss-animate"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 } satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
